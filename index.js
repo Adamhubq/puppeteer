fbd3a44fd76347948878b1458b377b9d0cb9372c
Adamhubq <rustam.osmanov.90@inbox.ru>
2019-10-07 17:58:46 +0300

    asdas

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-const puppeter = require('puppeteer');
+const puppeteer = require('puppeteer');
 
-puppeter.launch()
-    .then(browser => new Promise((resolve, reject) => {
-        browser.newPage()
-            .then(page => page.goto('https://220-volt.ru'))
-        then(resp => page.screenshot({
-            path: 'example.png'
-        }))
-    }))
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto('https://krasnodar.kerama-marazzi.com/ru/production/');
+
+
+    let response = await page.$$('.image');
+
+    let index = response.length
+    
+    const divsCounts = await page.$$eval('.image', divs => divs.length);
+
+
+
+    // 
+    // const html = await page.$eval('.main-container', e => e.outerHTML);
+    // for (; index--;) {
+    //     console.log(await response[index].getProperty('href')alue)
+    // }
+
+    await browser.close();
+})();
+
+// downloads
+// var fs = require('fs'),
+//     request = require('request');
+
+// var download = function (uri, filename, callback) {
+//     request.head(uri, function (err, res, body) {
+//         request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+//     });
+// };
+
+// download('https://images2.yesmagazine.ru/upload/cache_img/eb0/eb0eb1b749f48248f750d21052123f3f_fitted_660x880.jpg', 'google.png', () => {
+//     console.log('done');
+// });
